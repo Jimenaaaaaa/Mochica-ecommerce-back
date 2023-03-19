4684112ac29e2e96dd656d927f6b93764c389c8d
Jimena <jimenanrp2@gmail.com>
2023-03-19 20:59:46 +0100

    Add type to Product entity

--- src/entities/product.ts
+++ src/entities/product.ts
@@ -1,5 +1,13 @@
 import { Artist } from './artist';
 
+export type AddedProduct = {
+  name: string;
+  price: number;
+  cone: number;
+  size: string;
+  author: Artist;
+};
+
 export type Product = {
   id: string;
   name: string;
