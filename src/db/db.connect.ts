1b0049af3e07882d2bf5eb82dcd51777c3053400
Jimena <jimenanrp2@gmail.com>
2023-03-20 15:32:39 +0100

    Erase console.log code line

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -5,13 +5,11 @@ const { cluster, user, password, dbName } = config;
 import createDebug from 'debug';
 const debug = createDebug('FP: dbConnect');
 
-
 export const dbConnect = (env?: string) => {
   debug('enter function');
   const finalEnv = env || process.env.NODE_ENV;
   const finalDBName = finalEnv === 'test' ? dbName + '_testing' : dbName;
 
   const uri = `mongodb+srv://${user}:${password}@${cluster}/${finalDBName}?retryWrites=true&w=majority`;
-  console.log(uri);
   return mongoose.connect(uri);
 };
