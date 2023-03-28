47109021588fef1aa441578628b0ee4a70ae7f2b
Jimena <jimenanrp2@gmail.com>
2023-03-28 09:33:18 +0200

    Erase unused code

--- src/routers/artists.router.ts
+++
@@ -1,6 +0,0 @@
-import { Router as router } from 'express';
-
-// Lo sigo luego
-// export const artistsRouter = router();
-// const controller = new ArtistsController(repo);
-// artistsRouter.get('/', controller.getAll.bind(controller));
